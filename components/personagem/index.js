c4431c8ef79f14e4334deb5ca33f9b1bc869f8a2
Alexsander <alexsanderdeoliveira@gmail.com>
2023-11-16 19:23:14 -0300

    add lista consulta nos personagens api

--- components/personagem/index.js
+++ components/personagem/index.js
@@ -1,23 +1,51 @@
 import styles from './index.module.scss';
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+import { exibirMensagem } from '../../services/toastr-service';
 
 export default function Personagem() {
-    return <div className={`${styles['personagem-item']}  container`} >
-            <div className={styles['personagem-conteudo']}>
-                <div className={styles['personagem-imgs']}>
-                    <img src="assets/icones/bojack-icone.png" />
-                    <img src="assets/icones/daiane-icone.png" />
-                    <img src="assets/icones/tod-icone.png" />
-                    <img src="assets/icones/peanutbutter-icone.png" />
+    const [personagens, setPersonagens] = useState([]);
+
+    useEffect(() => {
+        listarPersonagens();
+    }, []);
+
+    const listarPersonagens = () =>  {
+      api.get('personagens')
+        .then(res => {
+            setPersonagens(res.data.data);
+        }).catch(res => {
+            let mensagem = 'Ocorreu um erro tente novamente!';
+            if ( res.response && res.response.status && res.response.status === 400 ) {
+                mensagem = res.response.data.errors.join(',');
+            }
+            exibirMensagem(mensagem, 'error');
+        });
+    };
+
+    return <>
+        { 
+            personagens.map(personagem => {
+                return <div className={`${styles['personagem-item']}  container`} key={personagem.id} >
+                    <div className={styles['personagem-conteudo']}>
+                        <div className={styles['personagem-imgs']}>
+                            <img src="assets/icones/bojack-icone.png" />
+                            <img src="assets/icones/daiane-icone.png" />
+                            <img src="assets/icones/tod-icone.png" />
+                            <img src="assets/icones/peanutbutter-icone.png" />
+                        </div>
+                        <h2 className="titulos">Personagens</h2>
+                        <h3>{personagem.nome}</h3>
+                        <p>
+                            {personagem.descricao}
+                        </p>
+                    </div>
+                    <div className={styles['personagem-imagem']}>
+                        <img alt="Diane Nguyen" src={personagem.icone} />
+                    </div>
                 </div>
-                <h2 className="titulos">Personagens</h2>
-                <h3>Diane Nguyen</h3>
-                <p>
-                    Ela é a caneta de BoJack. Uma intelectual incompreendida que fugiu de sua família em Boston , ela se descreve como uma feminista da terceira onda . Ela e BoJack formam uma forte amizade , até que seu relacionamento se torna tenso depois que BoJack começa a ter sentimentos por ela.
-                </p>
-            </div>
-            <div className={styles['personagem-imagem']}>
-                <img alt="Diane Nguyen" src="assets/daiane-personagem.png" />
-            </div>
-        </div>
+            })
+        }
+    </>
 }
 
